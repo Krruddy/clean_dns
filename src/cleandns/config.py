5d051f6da1d26e09b3c78281c8c850cc0f89af7b
Krruddy <rkinsiclounon@gmail.com>
2026-02-27 11:20:50 +0100

    Forgot to add the config class

---
+++ src/cleandns/config.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class DNSConfig:
+    """Configuration options for DNS file processing."""
+    omit_origin: bool = False
+    human_readable: bool = False
+    omit_ttl: bool = False
+    omit_record_ttl: bool = False
